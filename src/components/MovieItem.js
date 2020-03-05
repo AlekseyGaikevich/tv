9984a9e23710c4f6bb2b24bab7b34adf4cacaf35
AlekseyGaikevich <alekseygaikevich@gmail.com>
2020-03-05 22:58:26 +0300

    some changes

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const MovieItem = (props) => {
-    console.log(props)
-    const {title, voteAverage, image, overview} = props.data;
-    return (
-        <div>
-            <h3>{title}</h3>
-            <img src={image} alt={title} />
-            <p>{voteAverage}</p>
-            <p>{overview}</p>
-        </div>
-    )
+class MovieItem extends Component {
+    render() {
+        const { data: { title, voteAverage, image, overview } } = this.props;
+        console.log(this);
+        return (
+            <div>
+                <h3>{title}</h3>
+                <img src={image} alt={title} />
+                <p>{voteAverage}</p>
+                <p>{overview}</p>
+            </div>
+        );
+    };
 };
 
 export default MovieItem;
